8d82960a8366c62bf234663cf4d1de4c4c4e142f
jonanOribe <jonan.oribe@gmail.com>
2020-10-31 12:10:46 +0100

    Update main_classes.py

--- main_classes.py
+++ main_classes.py
@@ -1,6 +1,5 @@
 class Company:
     def __init__(self,name,website,type):
-
-    self.name=name
-    self.website=website
-    self.type=type
+        self.name=name
+        self.website=website
+        self.type=type

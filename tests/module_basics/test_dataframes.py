9a44ae81d8aae2d974056bcd87e01c4debbe0770
jonanOribe <jonan.oribe@gmail.com>
2020-11-01 12:54:12 +0100

    Refractor

--- tests/module_basics/test_dataframes.py
+++ tests/module_basics/test_dataframes.py
@@ -4,19 +4,10 @@ def test_text_cleaner():
     web_content='_This_* is a good place to start messing up thing like  #__!'
     assertion_value=text_cleaner(web_content).rstrip()
     example_text=' This   is a good place to start messing up thing like  '.rstrip()
-    assert assertion_value == example_text
-    assert assertion_value != web_content
+    assert assertion_value == example_text and assertion_value != web_content
 
 def test_dataframe_percent_and_points():
-    df_len_1=30
-    df_len_2=36
-    first_slice,second_slice,third_slice=dataframe_percent_and_points(df_len_1)
-    first_slice_2,second_slice_2,third_slice_2=dataframe_percent_and_points(df_len_2)
-    assert first_slice == 3
-    assert second_slice == 6
-    assert third_slice == 9
-    assert third_slice != 10
-    assert first_slice_2 == 4
-    assert second_slice_2 == 7
-    assert third_slice_2 == 11
-    assert third_slice_2 != 15
+    df_len_1,df_len_2=30,36
+    points_ranges,points_ranges_2=dataframe_percent_and_points(df_len_1),dataframe_percent_and_points(df_len_2)
+    assert (max(points_ranges[0]) == 2) and (max(points_ranges[1]) == 5) and (max(points_ranges[2]) == 8) and (max(points_ranges[3]) != 10)
+    assert (max(points_ranges_2[0]) == 3) and (max(points_ranges_2[1]) == 6) and (max(points_ranges_2[2]) == 10) and (max(points_ranges_2[3]) != 15)
